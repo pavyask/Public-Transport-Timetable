bdebb8f01bd93f9b57a67a3f2b188517abecd9ab
pavyask <pavel.yasko03@gmail.com>
2022-12-26 14:00:04 +0100

    Added User model

---
+++ Public-Transport-Timetables/Minimal-Web-API/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Minimal_Web_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
